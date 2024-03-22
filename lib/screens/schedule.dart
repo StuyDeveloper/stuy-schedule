64352a8eab3317748d2f5ccf226b801edc2adb93
ONLYIWIN21 <flintthecuber@gmail.com>
2024-03-21 21:19:57 -0400

    text box for schedule page

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -22,6 +22,29 @@ class ScheduleTabState extends State<ScheduleTab> {
         CupertinoSliverNavigationBar(
           largeTitle: Text('Schedule'),
         ),
+        SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            <Widget>[
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  color: CupertinoColors.systemFill,
+                  borderRadius: BorderRadius.all(Radius.circular(60)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Center(
+                    child: Text(
+                      'No schedule available',
+                      style: TextStyle(
+                        color: CupertinoColors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
